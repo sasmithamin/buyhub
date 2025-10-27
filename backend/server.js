96e494b805dacd199c21e8180ef82384aed3e564
sasmithamin <sasmithaminidu123@gmail.com>
2025-10-28 00:02:59 +0530

    firstt

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import productRoutes from "./routes/productRoutes";
+
