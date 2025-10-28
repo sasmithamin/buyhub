1b6a2713b34857d53e3316597145dbd330d5c040
sasmithamin <sasmithaminidu123@gmail.com>
2025-10-28 19:46:16 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,28 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes";
 
+dotenv.config();
+connectDB();
+const app = express();
+
+app.use(cors());
+app.use(express.json()); 
+
+app.get("/", (req, res) => {
+    res.send("BuyHub API running");
+});
+
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ Message: err.Message });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>  console.log('Server running from ${PORT}'));
