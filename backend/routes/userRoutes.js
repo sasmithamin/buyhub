4852e25e43c56d6c93e09ef86d2801c338a113a2
sasmithamin <sasmithaminidu123@gmail.com>
2025-10-31 22:35:49 +0530

    user models and routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,28 @@
 import express from "express";
-const router = express.Router();
+import User from "../models/userModel.js";
 
-router.get("/test", (req, res) => {
-  res.json({ message: "User route is working!" });
+const router =  express.Router();
+
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return res.status(400).json({ message: "User alrady exists"});
+  }
+  
+  const user =  await User.create({ namee, email, password});
+
+  if(user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400).json({ message: "Invalid user data"});
+  }
 });
 
 export default router;
