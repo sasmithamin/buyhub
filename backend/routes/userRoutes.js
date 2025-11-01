30e2cc41d9db75bfe5fc0e326c89529bdac3c108
sasmithamin <sasmithaminidu123@gmail.com>
2025-11-01 22:07:51 +0530

    user login and generate JWT

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
+import bcrypt from "bcryptjs";
 
 const router =  express.Router();
 
+//register
 router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -22,6 +25,25 @@ router.post("/register", async (req, res) => {
     });
   } else {
     res.status(400).json({ message: "Invalid user data"});
+  }
+});
+
+//logins
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user =  await User.findOne({ email });
+
+  if (user && (await  bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401).json({  message: "Invalid  email or  password" });
   }
 });
 
