d04c4772514aea5e0fb664c3a28742b27ff02b51
sasmithamin <sasmithaminidu123@gmail.com>
2025-11-02 20:04:16 +0530

    JWT middleware and protected the routes using token

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import User from "../models/userModel.js";
 import generateToken from "../utils/generateToken.js";
 import bcrypt from "bcryptjs";
+import { protect} from "../middleware/authMiddleware.js";
 
 const router =  express.Router();
 
@@ -14,7 +15,7 @@ router.post("/register", async (req, res) => {
     return res.status(400).json({ message: "User alrady exists"});
   }
   
-  const user =  await User.create({ namee, email, password});
+  const user =  await User.create({ name, email, password});
 
   if(user) {
     res.status(201).json({
@@ -45,6 +46,29 @@ router.post("/login", async (req, res) => {
   } else {
     res.status(401).json({  message: "Invalid  email or  password" });
   }
+});
+
+//profile
+router.get("/profile", protect, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+
+    if (user) {
+      req.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin:  user.isAdmin,
+      });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+
+  } catch (error) {
+    console.error("Profile route error:", error.message);
+    res.status(500).json({ message: "  Server error"});
+  }
+  
 });
 
 export default router;
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expireIn: "30d",
+        expiresIn: "30d",
     });
 }
 export default generateToken;
